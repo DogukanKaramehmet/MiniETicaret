327f528161ced5aceaa539e8318bf37e5adb083b
Doğukan Karamehmet <dogukan6186@gmail.com>
2024-12-04 14:52:25 +0300

    uygulama yuklendi

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -0,0 +1,37 @@
+﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Contexts
+{
+    public class ETicaretAPIDbContext : DbContext
+    {
+        public ETicaretAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Customer> customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //ChangeTracker: Entityler üzerinden yapılan değşiklerin ya da yeni eklenen verilerin yakalanmasını sağlayan propertdir. Track edilen verileri yaklayıp elde etmemizi sağlıyor 
+            var datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdateDate = DateTime.UtcNow
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.ViewModels.Products;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(_productReadRepository.GetAll());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Product model)
+        {
+            _productWriteRepository.AddAsync();
+            return Ok();
+        }
+
+
+    }
+}
